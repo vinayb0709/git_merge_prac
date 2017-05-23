fe2e7a7e34d1764557958d4f98a56de5a44010e4
Vinay Bantupalli <vinaybantupalli@gmail.com>
2017-05-23 08:56:37 +0530

    Started real life applications

---
+++ 1/q1.py
@@ -0,0 +1,7 @@
+class Car():
+	"""docstring for Car"""
+	def __init__(self,color_name = None,company_name,model_name = None,gear_num = None):
+		self.color = color_name
+		self.company = company_name
+		self.model = model_name
+		self.gear = gear_num
